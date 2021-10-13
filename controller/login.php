2b2c7e87a8d64722703c4c205f060337947715ba
Hailong0962 <hailong0962@gmail.com>
2021-10-13 22:46:11 +0700

    update password_hash

--- controller/login.php
+++ controller/login.php
@@ -10,53 +10,57 @@
 			$this->viewadmin("login",[]);
 		}
 		public function login(){
-			$result_mess = false;
-			if (isset($_POST["submit"])) {
-				$username = $_POST["username"];
-				$password_input = $_POST["password"];
-				
-				if ( empty($_POST["username"]) || empty($_POST["password"])) {
-					$this->viewadmin("login",[
-						"result"=>$result_mess,
-					]);
-				}else{
-					$kq = $this->login_model->login($username,$password_input);
-					$usernameDB = '';
-					$passwordDB = '';
-					$rollDB = '';
-					$id = '';
-					foreach ($kq as $key => $ketqua) {
-						$id = $ketqua["id_account"];
-						$usernameDB = $ketqua["username"];
-						$passwordDB = $ketqua["password"];
-						$rollDB = $ketqua["roll"];
-					}
-					if ( $username === $usernameDB && $password_input === $passwordDB) {
-						
-						if ($rollDB == "user") {
-							$_SESSION["id"] = $id;
-							if (!empty($_POST["remeber"])) {
-								setcookie("id", "username", time()+3600, "/", "", 0);
+
+	 		$result_mess = false;
+
+	 		if (isset($_POST["submit"])) {
+	 			$username = $_POST['username'];
+	 			$password_input = $_POST['password'];
+	 			
+
+	 			if(empty($_POST["username"]) || empty($_POST["password"])) {
+	 				$this->viewadmin("login",[
+	 					"result" => $result_mess,
+	 				]);
+	 			}
+	 			$result = $this->login_model->login($username);
+	 			
+	 			
+	 				if (mysqli_num_rows($result)) {
+		 				while($row = mysqli_fetch_array($result)){
+		 					$id = $row["id_account"];
+		 					$username = $row["username"];
+		 					$password = $row["password"];
+		 					$rollDB = $row["roll"];
+		 				}
+		 				if (password_verify($password_input, $password)) {
+		 					if ($rollDB == "user") {
+								$_SESSION["id"] = $id;
+								if (!empty($_POST["remeber"])) {
+									setcookie("id", "username", time()+3600, "/", "", 0);
+								}
+								header("Location: http://localhost/login-project/trangchu");
+							}else if($rollDB == "admin"){
+								$_SESSION["id"] = $id;
+								$_SESSION["username"] = $username;
+								if (!empty($_POST["remeber"])) {
+									setcookie("id", "username", time()+3600, "/", "", 0);
+								}
+								
+								header("Location: http://localhost/login-project/home");
 							}
-							
-							header("Location: http://localhost/login-project/trangchu");
-						}else if($rollDB == "admin"){
-							$_SESSION["id"] = $id;
-							$_SESSION["username"] = $username;
-							if (!empty($_POST["remeber"])) {
-								setcookie("id", "username", time()+3600, "/", "", 0);
-							}
-							
-							header("Location: http://localhost/login-project/home");
-						}
-					}else{
-						$this->viewadmin("login",[
-	 						"result" => $result_mess,
-						]);
-					}
-				}
-			}
-		}
+		 				}else{
+		 					$this->viewadmin("login",[
+		 						"result" => $result_mess,
+							]);
+		 				}
+		 			}else{
+	 				$this->viewadmin("login",[
+	 					"result" => $result_mess,
+				 	]);
+	 			}
+	 		}
+		}	
 		public function logout(){
 			session_destroy();
 			unset($_SESSION['username']);
--- model/login_model.php
+++ model/login_model.php
@@ -6,8 +6,8 @@
 	class login_model extends DB
 	{
 		
-		public function login($username,$password){
-			$sql = "SELECT * FROM tbl_user WHERE username = '$username' AND password = '$password' ";
+		public function login($username){
+			$sql = "SELECT * FROM tbl_user WHERE username = '$username' limit 1 ";
 			return mysqli_query($this->conn, $sql);
 		}
 
